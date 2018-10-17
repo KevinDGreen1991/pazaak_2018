f92ff81165c75738c282f5e6ea335592c125d18a
kevin.green630 <kevin.green630@myci.csuci.edu>
2018-10-17 11:39:26 -0700

    Implemented basic card class.

--- app/src/main/java/com/pazaak/prototype/pazaakprototype/Card.java
+++ app/src/main/java/com/pazaak/prototype/pazaakprototype/Card.java
@@ -1,15 +1,21 @@
 package com.pazaak.prototype.pazaakprototype;
 
+import android.graphics.drawable.Drawable;
+
 public class Card {
     private int cardId;
     private int cardType;
     private int cardValue;
-    public static int PLUS = -1;
-    public static int MINUS = -2;
-    public static int PM = -3;
+    public static final int PLUS = -1;
+    public static final int MINUS = -2;
+    public static final int PM = -3;
+    public static final int MAIN = -4;
     public Card(int type, int value){
         this.cardType = type;
-        this.cardValue = value;
+        if((this.cardType == MAIN && (value < 1 || value > 10)) || ((this.cardType == PLUS || this.cardType == MINUS || this.cardType == PM) && (value < 1 || value > 6)))
+            this.cardValue = 1;
+        else
+            this.cardValue = value;
 
     }
     public int getType()
@@ -19,5 +25,20 @@ public class Card {
     public int getValue()
     {
         return this.cardValue;
+    }
+    public int getImage()
+    {
+        switch (this.cardType){
+            case PLUS: switch(this.cardValue){
+                case 1: return R.drawable.plus1;
+                case 2: return R.drawable.plus2;
+                case 3: return R.drawable.plus3;
+                case 4: return R.drawable.plus4;
+                case 5: return R.drawable.plus1;
+            }
+
+
+            default: return 0;
+        }
     }
 }
