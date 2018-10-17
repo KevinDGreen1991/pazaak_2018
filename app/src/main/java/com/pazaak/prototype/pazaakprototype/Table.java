b1f9b5708299829810411c01e66955ef9a9d6328
luke.cervantes576 <luke.cervantes576>
2018-10-17 13:41:37 -0700

    Added Some code to Table for Cards

--- app/src/main/java/com/pazaak/prototype/pazaakprototype/Table.java
+++ app/src/main/java/com/pazaak/prototype/pazaakprototype/Table.java
@@ -7,6 +7,8 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
 
+import java.util.Random;
+
 public class Table extends AppCompatActivity
 {
     @Override
@@ -15,6 +17,7 @@ public class Table extends AppCompatActivity
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_table);
 
+        Random generator = new Random();
         Card[] board1 = new Card[9];
         Card[] board2 = new Card[9];
         Card[] MainDeck = new Card[40];
@@ -28,8 +31,31 @@ public class Table extends AppCompatActivity
 
         int val = myCard.getValue();
 
-        ImageView img = (ImageView) findViewById(R.id.p1Slot0);
-        img.setImageResource(R.drawable.minus3);
+        for (int i = 0; i < board1.length; i++)
+        {
+            board1[i] = MainDeck[generator.nextInt(40)];
+        }
+
+
+        ImageView board1slot0 = (ImageView) findViewById(R.id.p1Slot0);
+        ImageView board1slot1 = (ImageView) findViewById(R.id.p1Slot1);
+        ImageView board1slot2 = (ImageView) findViewById(R.id.p1Slot2);
+        ImageView board1slot3 = (ImageView) findViewById(R.id.p1Slot3);
+        ImageView board1slot4 = (ImageView) findViewById(R.id.p1Slot4);
+        ImageView board1slot5 = (ImageView) findViewById(R.id.p1Slot5);
+        ImageView board1slot6 = (ImageView) findViewById(R.id.p1Slot6);
+        ImageView board1slot7 = (ImageView) findViewById(R.id.p1Slot7);
+        ImageView board1slot8 = (ImageView) findViewById(R.id.p1Slot8);
+
+        board1slot0.setImageResource(board1[0].getImage());
+        board1slot1.setImageResource(board1[1].getImage());
+        board1slot2.setImageResource(board1[2].getImage());
+        board1slot3.setImageResource(board1[3].getImage());
+        board1slot4.setImageResource(board1[4].getImage());
+        board1slot5.setImageResource(board1[5].getImage());
+        board1slot6.setImageResource(board1[6].getImage());
+        board1slot7.setImageResource(board1[7].getImage());
+        board1slot8.setImageResource(board1[8].getImage());
 
     }
 }
